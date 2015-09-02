df55a270cb3577e1de0c558b1bb558518bb99958
Joonas Aalto <joonas.aalto@wunderdog.fi>
2015-09-02 22:34:20 +0300

    Render a button.

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,13 @@
-module Main where
+import Html exposing (div, input, button, text, Html)
+import Html.Events exposing (onClick)
+import StartApp.Simple as StartApp
+import Signal exposing (Signal, Address)
+
+--module Main where
 
 -- MODEL
 
 type alias Model = { }
-
 
 -- UPDATE
 
@@ -13,3 +17,20 @@ update : Action -> Model -> Model
 update action model =
   case action of
     Reset -> model
+
+-- VIEW
+
+--view : Address Action -> Model -> Html
+
+view address model =
+    div []
+        [ 
+            button [] [ text "jep"]
+            --input []
+        ]
+
+main =
+    StartApp.start
+    { model = {}
+    , update = update
+    , view = view }
