af73f2f1080a04fe288d0c14b9037052051e438d
Joonas Aalto <joonas.aalto@wunderdog.fi>
2015-11-24 22:43:28 +0200

    More date handling.

--- src/DateUtils.elm
+++ src/DateUtils.elm
@@ -1,23 +1,44 @@
 module DateUtils where
 
 import Date exposing (Date)
+import Time exposing (Time)
 import List exposing (indexedMap)
 
 weekDays : List (Int, String)
 weekDays =
     indexedMap (,) [ "Ma", "Ti", "Ke", "To", "Pe", "La", "Su" ]
 
-dates : Int -> List Int
-dates today =
-    List.map (\day -> date today day) weekDays
+weekDates : Time -> List Date
+weekDates currentTime =
+    List.map (\dayOffset -> add dayOffset currentTime)
+    (dateOffsets (timeToInt currentTime))
 
-date : Int -> (Int, String) -> Int
-date today weekDay =
-    today + (fst weekDay - today)
+timeToInt : Time -> Int
+timeToInt time =
+    (weekdayToInt << Date.dayOfWeek << Date.fromTime) time
+
+dateOffsets : Int -> List Int
+dateOffsets today =
+    List.map (\day -> dayIndex today day) weekDays
+
+dayIndex : Int -> (Int, String) -> Int
+dayIndex todayIndex weekDay =
+    todayIndex + (fst weekDay - todayIndex)
 
 -- Add days to timestamp
-add : Int -> Int -> Date
+add : Int -> Time -> Date
 add days time =
-    time + days * 1000 * 60 * 60 * 24
-    |> toFloat
+    time + (toFloat days) * 1000 * 60 * 60 * 24
+    -- |> toFloat
     |> Date.fromTime
+
+weekdayToInt : Date.Day -> Int
+weekdayToInt dow =
+    case dow of
+        Date.Mon -> 0
+        Date.Tue -> 1
+        Date.Wed -> 2
+        Date.Thu -> 3
+        Date.Fri -> 4
+        Date.Sat -> 5
+        Date.Sun -> 6
