2206e88713ddc7dae1a4e57f84ab5c929d8ed824
Joonas Aalto <joonas.aalto@wunderdog.fi>
2015-12-21 22:21:49 +0200

    Create serial primary keys.

--- tables.sql
+++ tables.sql
@@ -1,10 +1,13 @@
+drop table if exists project;
+drop table if exists hour_entry;
+
 create table project(
-    id      int,
+    id      serial primary key,
     name    varchar(200)
 );
 
 create table hour_entry(
-    id      int,
+    id      serial primary key,
     date    date,
     hours   real
 );
