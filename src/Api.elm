af7a40ad103eb2bcd469a2db64cdb07c99decd04
Joonas Aalto <joonas.aalto@wunderdog.fi>
2016-01-15 00:14:37 +0200

    Remove crossOriginGet function.

--- src/Api.elm
+++ src/Api.elm
@@ -7,23 +7,9 @@ import Date exposing (Date)
 
 import Model exposing (Project, HourEntry)
 
--- "localhost:3000/project?id=eq.1&select=*,hour_entry{*}"
 getProjects : String -> Task Error (List Project)
 getProjects query =
-    get decodeProjects "/project?select=id,name,hour_entry{*}"
-
-get : Json.Decoder (List Project) -> String -> Task Error (List Project)
-get decoder url =
-    fromJson decodeProjects (crossOriginGet url)
-
-crossOriginGet : String -> Task RawError Response
-crossOriginGet url =
-    send defaultSettings
-        { verb = "GET"
-        , headers = []
-        , url = url
-        , body = empty
-        }
+    Http.get decodeProjects "/project?select=id,name,hour_entry{*}"
 
 decodeProjects : Json.Decoder (List Project)
 decodeProjects =
