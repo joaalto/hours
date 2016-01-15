e4be64b21cdcf16d860455dcedab31bbaa172d95
Joonas Aalto <joonas.aalto@wunderdog.fi>
2016-01-15 22:31:41 +0200

    Cleaning up.

--- src/Main.elm
+++ src/Main.elm
@@ -37,29 +37,37 @@ port tasks : Signal (Task.Task Never ())
 port tasks =
     app.tasks
 
-currentTime : Time -> String
-currentTime t =
-    let date' = fromTime t
-        hour' = pad (hour date')
-        minute' = pad (minute date')
-        second' = pad (second date')
-    in
-        hour' ++ ":" ++ minute' ++ ":" ++ second'
-
-pad : Int -> String
-pad = padLeft 2 '0' << toString
-
-timeSignal : Signal Action
-timeSignal =
-    every Time.second
-    |> Signal.map currentTime
-    |> Signal.map Update.UpdateTime
-
 init : String -> (Model, Effects Action)
 init query =
     ( initialModel
     , Update.getProjects query
     )
+
+initialModel : Model
+initialModel =
+    { time = ""
+    , currentDate = Date.fromTime startTime
+    , firstDayOfWeek = dayIndexToDate 0 (Date.fromTime startTime)
+    , projects = (Ok [])
+    }
+
+-- currentTime : Time -> String
+-- currentTime t =
+--     let date' = fromTime t
+--         hour' = pad (hour date')
+--         minute' = pad (minute date')
+--         second' = pad (second date')
+--     in
+--         hour' ++ ":" ++ minute' ++ ":" ++ second'
+--
+-- pad : Int -> String
+-- pad = padLeft 2 '0' << toString
+--
+-- timeSignal : Signal Action
+-- timeSignal =
+--     every Time.second
+--     |> Signal.map currentTime
+--     |> Signal.map Update.UpdateTime
 
 -- actions : Mailbox Action
 -- actions =
@@ -77,35 +85,3 @@ init query =
 -- port requests =
 --     Signal.map Api.getProjects queries.signal
 --         |> Signal.map (\task -> Task.toResult task `andThen` Signal.send results.address)
-
--- initialModel : (Model, Effects Action)
-initialModel : Model
-initialModel =
-    { time = ""
-    , currentDate = Date.fromTime startTime
-    , firstDayOfWeek = dayIndexToDate 0 (Date.fromTime startTime)
-    , projects = (Ok [])
-    }
-        -- Api.projects
-        -- [
-        --     { id = 1
-        --     , name = "Eka projekti"
-        --     , hourEntries = []
-        --         -- [ { date =
-        --         --   , hours = 7.5
-        --         --   }
-        --         -- , { dayOfWeek = 3
-        --         --   , hours = 7.5
-        --         --   }
-        --         -- ]
-        --     },
-        --     { id = 2
-        --     , name = "Toka projekti"
-        --     , hourEntries = []
-        --         -- [ { dayOfWeek = 4
-        --         --   , hours = 3.5
-        --         --   }
-        --         -- ]
-        --     }
-        -- ]
-    -- }
