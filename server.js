31af30893e8b0e7009b17aa322dc18f101d4d3ab
Joonas Aalto <joonas.aalto@wunderdog.fi>
2016-01-21 00:32:24 +0200

    Renamed varibles.

--- server.js
+++ server.js
@@ -6,38 +6,38 @@ const app = express()
 
 app.use('/', express.static(__dirname + '/static'));
 
-app.get('/project', (request, response) => {
+app.get('/project', (req, res) => {
     superagent
-        .get('localhost:3000/project?select=' + request.query.select)
-        .end((err, res) => response.send(res.body));
+        .get('localhost:3000/project?select=' + req.query.select)
+        .end((err, response) => res.send(response.body));
 });
 
-app.post('/hour_entry', jsonParser, (request, response) => {
+app.post('/hour_entry', jsonParser, (req, res) => {
     superagent
         .post('localhost:3000/hour_entry')
-        .set('Prefer', request.get('Prefer'))
-        .send(request.body)
-        .end((err, res) => {
+        .set('Prefer', req.get('Prefer'))
+        .send(req.body)
+        .end((err, response) => {
             if(!err) {
-                response.send(res.body);
+                res.send(response.body);
             }
             else {
-                response.status(err.status).send(err.response);
+                res.status(err.status).send(err.res);
             }
         });
 });
 
-app.patch('/hour_entry', jsonParser, (request, response) => {
+app.patch('/hour_entry', jsonParser, (req, res) => {
     superagent
-        .patch(`localhost:3000/hour_entry?date=${request.query.date}&project_id=${request.query.project_id}`)
-        .set('Prefer', request.get('Prefer'))
-        .send(request.body)
-        .end((err, res) => {
+        .patch(`localhost:3000/hour_entry?date=${req.query.date}&project_id=${req.query.project_id}`)
+        .set('Prefer', req.get('Prefer'))
+        .send(req.body)
+        .end((err, response) => {
             if(!err) {
-                response.send(res.body);
+                res.send(response.body);
             }
             else {
-                response.status(err.status).send(err.response);
+                res.status(err.status).send(err.res);
             }
         });
 });
